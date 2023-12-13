f458bd1b3af4c9cfab961832587779bf26d980b2
MuhammadAhmad-dot <ahmadrj049@gmail.com>
2023-12-13 23:10:18 +0500

    recommendation fix

--- FYP1-S23-006-D-CyberLearn-Code/frontend/src/page/dashboard/Recommend/recommended.jsx
+++ FYP1-S23-006-D-CyberLearn-Code/frontend/src/page/dashboard/Recommend/recommended.jsx
@@ -9,7 +9,7 @@ const Enrolled = ({ onSelectCourse }) => {
 
     useEffect(() => {
         async function fetchEnrolledCourses() {
-            const username = localStorage.getItem("username");
+            const username = localStorage.getItem("username");3
             if (!username) {
                 console.log("No username found");
                 return;
